98d515047498193033f1ba180add7f40a2d9395c
Jacob Matta <jpm86668@uga.edu>
2023-02-08 20:39:58 -0500

    visualization exercise

--- Visualization_exercise_code.R
+++ Visualization_exercise_code.R
@@ -2,6 +2,7 @@
 library(here) #to set paths
 library(readr) #for loading csv files
 library(ggplot2)
+library(plotly)
 
 #uploading data 
 data_location <- here::here("data","raptor_by_nba_player.csv")
@@ -14,11 +15,29 @@ str(rawdata) #used to see how data is structured
 #extracting only players in the 2022 season 
 rawdata_2022 <- rawdata[rawdata$season == '2022', ]
 
-#removing all columns but defensive RAPTOR and offensive RAPTOR
-#keeping column numbers 1,3,12,13 = player name, year, defensive raptor and offensive raptor
-clean_data <- rawdata_2022[ -c(2,4:11, 14:21) ]
+#the fivethirtyeight scatter plot is only keeping players who have played 1042 minutes or more
+#removing players who have played less than 1042 minutes 
+clean_data <- rawdata_2022[rawdata_2022$mp > 1042,]
 
 #making ggplot scatterplot to match professional scatterplot 
-ggplot(rawdata_2022, aes(x=clean_data$raptor_offense, y=clean_data$raptor_defense)) +
-  geom_point() + xlim(-10,10) + ylim(-10,10)
+#shape controls dot shape
+#fill adds white filling to dot 
+#size controls the size of the dot 
+#the stroke controls the stroke of the black outline 
+g1 <- ggplot(clean_data, aes(x=raptor_offense, y=raptor_defense)) +
+  geom_rect(aes(xmin=-10,xmax=0,ymin=-10,ymax=0),fill="pink",alpha=0.01) +
+  geom_rect(aes(xmin=0,xmax=10,ymin=0,ymax=10),fill='turquoise',alpha=0.01) +
+  labs(x = "Offensive RAPTOR rating", y = "Defensive RAPTOR rating")
+
+g2 <- g1 + geom_point(shape = 21, colour = "black", fill = "white", size = 4, stroke = 1 ) + xlim(-10,10) + ylim(-10,10)
+
+g3 <- g2 + theme(panel.grid.minor = element_blank()) +  annotate(geom="label", x=-9,y=10,label="- offensive", fill="pink") +
+  annotate(geom="label", x=-9,y=8,label="+ defensive", fill='turquoise') +
+  annotate(geom="label", x=-9,y=-10,label="- defensive", fill='pink') +
+  annotate(geom="label", x=-9,y=-8,label="- offensive", fill='pink') + 
+  annotate(geom="label", x=9,y=10,label="+ offensive", fill='turquoise') +
+  annotate(geom="label", x=9,y=8,label="+ defensive", fill='turquoise') +
+  annotate(geom="label", x=9,y=-8,label="+ offensive", fill='turquoise') +
+  annotate(geom="label", x=9,y=-10,label="- defensive", fill='pink')
+g3
 
