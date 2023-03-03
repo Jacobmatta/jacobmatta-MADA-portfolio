124df77799db2ac359222498688d3e2c02a6cd8c
Jacob Matta <jpm86668@uga.edu>
2023-03-03 16:14:19 -0500

    creating models for week 8 exercise

--- Fluanalysis_exercise/code/Fitting.R
+++ Fluanalysis_exercise/code/Fitting.R
@@ -1,24 +1,62 @@
+#install
+install.packages("tidymodels")
+install.packages("broom.mixed")
+
+
 #library 
 library(dplyr)
 library(here)
+library(tidymodels)
+library(readr)
+library(broom.mixed)
+
 
 #loading clean data
 location = here("Fluanalysis_exercise", "data", "cleandata", "cleandata.RDS")
 clean_data = readRDS(location)
 
 #Fitting main predictor of interest to outcome to simple linear regression model
-lm1 = lm(formula = BodyTemp ~ SwollenLymphNodes, data = clean_data)
+#following the tidymodels procedure for linear regression https://www.tidymodels.org/start/models/
+linear_reg() %>% 
+  set_engine("lm")
+lm_mod = linear_reg()
+
+lm_fit = lm_mod %>% 
+  fit(BodyTemp ~ RunnyNose, data = clean_data )
+lm_fit
+
 
 #Fitting bodytemp with all predictors of interest 
-lm2 = lm(formula = BodyTemp ~ SwollenLymphNodes + Insomnia + Nausea, data = clean_data)
+linear_reg() %>% 
+  set_engine("lm")
+lm_mod2 = linear_reg()
+lm_fit2 = lm_mod %>% 
+  fit(formula = BodyTemp ~ RunnyNose + Insomnia + Nausea + SwollenLymphNodes, data = clean_data)
+lm_fit2
 
 #comparing the results from simple linear regression between body weight and swollen lymph nodes 
 #to multiple linear regression between body weight and Swollen Lymph Nodes + Insomnia + Nausea
-coef(lm1)
-coef(lm2)
-#It appears that the coefficent for the slope between body weight and Swollen Lymph Nodes becomes 
-#more negative when going from the linear regression model to the multiple linear regression model 
+tidy(lm_fit)
+tidy(lm_fit2)
 
-#fit log regression Nausea(outcome) vs Swollen Lymph Nodes
+#fit logistic regression Nausea(outcome) vs Swollen Lymph Nodes
+logistic_reg() %>% 
+  set_engine("glm")
+glm_mod = logistic_reg()
+glm_fit = glm_mod %>% 
+  fit(formula = Nausea ~ RunnyNose, data = clean_data)
+glm_fit
 
-#fit multile log regression for all redictors of interst 
+#fit multiple log regression for all predictors of interest 
+logistic_reg() %>% 
+  set_engine("glm")
+glm_mod = logistic_reg()
+glm_fit2 = glm_mod %>% 
+  fit(formula = Nausea ~ RunnyNose + SwollenLymphNodes + Insomnia, data = clean_data, family = binomial)
+glm_fit2
+#comparing the two logistic models 
+tidy(glm_fit)
+tidy(glm_fit2)
+
+
+
