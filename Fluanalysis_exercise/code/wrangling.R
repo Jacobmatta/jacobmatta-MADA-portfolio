7b14d4d7ce66c3b6d296efcb124199748ee8b0ce
Jacob Matta <jpm86668@uga.edu>
2023-03-12 22:34:29 -0400

    RDS file load code fix

--- Fluanalysis_exercise/code/wrangling.R
+++ Fluanalysis_exercise/code/wrangling.R
@@ -2,13 +2,28 @@
 library(tidyr)
 library(tidyselect)
 library(dplyr)
+library(here)
 
 #reading in the RDS file 
-setwd("~/Documents/R/jacobmatta-MADA-portfolio")
-list.files()
-rawdata = readRDS("~/Documents/R/jacobmatta-MADA-portfolio/Fluanalysis_exercise/rawdata/SympAct_Any_Pos.Rda") 
+datalocation = here("Fluanalysis_exercise","data", "rawdata", "SympAct_Any_Pos.Rda")
+rawdata = readRDS(datalocation)
 
 #Remove all variables that have Score or Total or FluA or FluB or Dxname or Activity in their name
-rawdata2 = rawdata[,!names(rawdata) %in% -c("Score", "Total", "FluA", "FluB", "Dxname", "Activity")]
-rawdata2 = subset(rawdata, select = -c("Score", "Total", "FluA", "FluB", "Dxname", "Activity"))
-rawdata2 = select(rawdata, - c("Score", "Total", "FluA", "FluB", "Dxname", "Activity"))
+rawdata2 = rawdata %>% select(-contains("Score")) %>% 
+  select(-contains("Total")) %>% 
+  select(-contains("FluA")) %>% 
+  select(-contains("FluB")) %>%
+  select(-contains("Dxname")) %>%
+  select(-contains("Activity")) %>%
+  select(-contains("Unique.Visit"))
+
+# Removing an NA values
+rawdata3 = na.omit(rawdata2)
+
+### Saving data as an RDS file
+#saving data as an RDS file in the cleandata folder as "cleandata.RDS"
+#used the here function to set the location to where I want to save the file 
+location = here("Fluanalysis_exercise", "data", "cleandata", "cleandata.RDS")
+
+#see if it worked
+readRDS(location)
